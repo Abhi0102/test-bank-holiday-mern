c27f30cb7f3e018931da5510bbaa55f1c83c1204
Abhi0102 <67204033+Abhi0102@users.noreply.github.com>
2021-12-25 11:10:45 +0530

    Again;

--- index.js
+++ index.js
@@ -21,12 +21,13 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use("/", routes);
-
 app.use(express.static("client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
+
+app.use("/", routes);
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error : ${err}`);
